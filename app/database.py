c6c948fe5d44b4f5fc76d777c50b17c9043b45e5
daoolet-HON <dauletkhabidullin@gmail.com>
2024-03-22 19:10:06 +0500

    updated

--- app/database.py
+++ app/database.py
@@ -9,3 +9,11 @@ engine = create_engine(SQLA_DB_URL, connect_args = {"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
